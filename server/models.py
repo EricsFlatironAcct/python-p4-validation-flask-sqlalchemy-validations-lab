2185bc8cb39275e340d47d9b7be2c196d81feb8c
Eric Krukowski <erickruk@amazon.com>
2023-09-16 22:46:56 -0400

    Lab complete

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if name=='':
+            raise ValueError("Must enter a name")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number)!= 10:
+            raise ValueError("must be 10 digits")
+        return phone_number
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +39,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('content')
+    def validate_content(self, key, content):
+        if (len(content)<250):
+            raise ValueError("Must have >=250 characters")
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if (len(summary)>=250):
+            raise ValueError("Must have >=250 characters")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        allowed = ['Fiction', 'Non-Fiction']
+        if category not in allowed:
+            raise ValueError("Invalid category")
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        allowed = ["Won't Believe", "Secret", "Top", "Guess"]
+        if title not in allowed:
+            raise ValueError("Title not clickbait-y")
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
